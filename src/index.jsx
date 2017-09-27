8a80657a4f4e616b208b64ddbb639c40ada5d006
Finn Pedersen <fpeders2@orange.fr>
2017-09-27 11:25:06 +0200

    added reducers

--- src/index.jsx
+++ src/index.jsx
@@ -8,9 +8,24 @@ import { createStore, combineReducers } from 'redux';
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+import messagesReducer from './reducers/messages_reducer';
+import channelsReducer from './reducers/channels_reducer';
+import currentUserReducer from './reducers/current_user_reducer';
+import selectedChannelReducer from './reducers/selected_channel_reducer';
+
 // State and reducers
+const initialState = {
+  messages: [],
+  channels: [ 'general', 'paris', 'react'],
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+}
+
 const reducers = combineReducers({
-  state: (state = {}, action) => state
+  messages: messagesReducer,
+  channels: channelsReducer,
+  currentUser: currentUserReducer,
+  selectedChannel: selectedChannelReducer
 });
 
 // render an instance of the component in the DOM
